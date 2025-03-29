f12f127dd2821718fbf295f4259b0803fd10904c
Sangiria <Kawaiinika@yandex.ru>
2025-03-29 17:26:36 +0300

    chore: cors

--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"clicker_api/routes"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 
@@ -10,7 +12,11 @@ import (
 
 func main() {
 	e := echo.New()
-
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS},
+	}))
+	
 	routes.InitEntryRoutes(e)
 
 	e.Start(":1323")
