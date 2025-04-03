d88a9f80a62bd4bba80b0891fce209b5c374b542
Sangiria <Kawaiinika@yandex.ru>
2025-04-03 22:46:51 +0300

    chore: изменен статус ответа

--- handlers/refresh.go
+++ handlers/refresh.go
@@ -13,7 +13,7 @@ func RefreshTokens(c echo.Context) error {
 	header := strings.Split(c.Request().Header.Get("Authorization"), " ")
 	id := service.ExtractIDFromToken(header[1], environment.GetVariable("REFRESH_TOKEN_SECRET"))
 
- 	return c.JSON(http.StatusCreated, map[string]interface{}{
+ 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"status": "0",
 		"tokens": map[string]string{
 			"access_token": service.NewToken(id, true),
