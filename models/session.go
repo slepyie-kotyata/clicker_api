4069d055bb8989ea57bbe66ec6ab5335859183c4
Sangiria <Kawaiinika@yandex.ru>
2025-04-18 02:41:45 +0300

    feat: добавлено поле dishes в session

--- models/session.go
+++ models/session.go
@@ -3,6 +3,7 @@ package models
 type Session struct {
 	ID         uint 		`json:"id" gorm:"primary_key"`
 	Money      uint			`json:"money"`
+	Dishes     uint         `json:"dishes"`
 	Upgrades   []Upgrade   	`json:"upgrades" gorm:"many2many:session_upgrades;"`
 	UserID     uint			`json:"user_id"`
 }
