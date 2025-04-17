4ce7c733ffedc942b300344ddd75de7299fa1790
Sangiria <Kawaiinika@yandex.ru>
2025-04-18 00:59:55 +0300

    fix: исправлена опечатка в where

--- handlers/game.go
+++ handlers/game.go
@@ -5,6 +5,7 @@ import (
 	"clicker_api/models"
 	"clicker_api/service"
 	"clicker_api/utils"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -14,7 +15,7 @@ func InitGame(c echo.Context) error {
 	id := service.ExtractIDFromToken(c.Request().Header.Get("Authorization"), environment.GetVariable("ACCESS_TOKEN_SECRET"))
 
 	var session models.Session
-	db.Preload("Upgrades.Boost").Where("user_id", id).First(&session)
+	db.Preload("Upgrades.Boost").Where("user_id = ?", id).First(&session)
 	if session.ID > 0 {
 		return c.JSON(http.StatusOK, map[string]interface{}{
 			"status": "0",
@@ -31,7 +32,7 @@ func InitGame(c echo.Context) error {
 	var first_upgrade models.Upgrade
 	db.Preload("Boost").Where("name = ?", "Гамбургер").First(&first_upgrade)
 	db.Model(&new_session).Association("Upgrades").Append(&first_upgrade)
-	
+
 	db.Preload("Upgrades.Boost").First(&new_session, new_session.ID)
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
