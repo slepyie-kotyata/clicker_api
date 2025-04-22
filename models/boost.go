630369a16cb14bc28810aa37a80c36f56bebf10a
Sangiria <Kawaiinika@yandex.ru>
2025-04-22 03:24:59 +0300

    chore: изменен тип поля Boost.Value

--- models/boost.go
+++ models/boost.go
@@ -5,6 +5,6 @@ type BoostType string
 type Boost struct {
 	ID			uint	 	`json:"id" gorm:"primary_key"`
 	BoostType	BoostType	`json:"boost_type" gorm:"type:boost_type"`
-	Value       uint		`json:"value"`
+	Value       float32		`json:"value"`
 	UpgradeID   uint		`json:"-"`
 }
