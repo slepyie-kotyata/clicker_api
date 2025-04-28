51c4369abb11689bde8a9b8f48bc026e5a0b432c
Sangiria <Kawaiinika@yandex.ru>
2025-04-28 05:43:09 +0300

    chore: временный вывод sql запросов

--- handlers/game.go
+++ handlers/game.go
@@ -200,8 +200,8 @@ func SellClick(c echo.Context) error {
 
 
 	db.Model(&session).Select("dishes", "money").Updates(models.Session{Dishes: session.Dishes - 1, Money: session.Money + uint(math.Ceil((total_money_per_click) * total_money_multiplier))})
-	db.Where("session_id = ?", session.ID).First(&level)
-	db.Model(&level).Select("xp").Updates(models.Level{XP: level.XP + 0.2})
+	db.Where("session_id = ?", session.ID).First(&level).Debug()
+	db.Model(&level).Select("xp").Updates(models.Level{XP: level.XP + 0.2}).Debug()
 
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
