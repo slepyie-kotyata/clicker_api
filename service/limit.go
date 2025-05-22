4fe40f8aa224c034fe5a495bf072fe0cab96116a
Sangiria <Kawaiinika@yandex.ru>
2025-05-22 22:38:38 +0300

    fix: limiter middleware

--- service/limit.go
+++ service/limit.go
@@ -1,1 +1,30 @@
 package service
+
+import (
+	"clicker_api/utils"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+func LimiterMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func (c echo.Context) error {
+			time_header := c.Request().Header.Get("X-timestamp")
+			request_time := time.UnixMilli(int64(utils.StringToUint(time_header)))
+			current_time := time.Now()
+
+			diff := current_time.Sub(request_time)
+
+			if diff >= time.Millisecond || diff < 0 {
+				return c.JSON(http.StatusForbidden, map[string]interface{}{
+					"status": 5,
+					"message": "request blocked",
+				})
+			}
+
+			return next(c)
+		}
+	}
+}
