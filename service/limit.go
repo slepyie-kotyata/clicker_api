cb0247fe7296bce5fdaf15f57621bf339abcdf90
Sangiria <Kawaiinika@yandex.ru>
2025-05-22 22:04:28 +0300

    chore: исправленный вывод разницы в консоль

---
+++ service/limit.go
@@ -0,0 +1,13 @@
+// package service
+
+// import "github.com/labstack/echo/v4"
+
+// func LimiterMiddleware() echo.MiddlewareFunc {
+// 	return func(next echo.HandlerFunc) echo.HandlerFunc {
+// 		return func (c echo.Context) error {
+// 			time_header := c.Request().Header.Get("X-timestamp")
+
+
+// 		}
+// 	}
+// }
