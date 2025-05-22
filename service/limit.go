9430bacf16bba45cf0bf6af30f205a118a25a6af
Sangiria <Kawaiinika@yandex.ru>
2025-05-22 22:07:10 +0300

    refactor: refactor

--- service/limit.go
+++ service/limit.go
@@ -1,13 +1,1 @@
-// package service
-
-// import "github.com/labstack/echo/v4"
-
-// func LimiterMiddleware() echo.MiddlewareFunc {
-// 	return func(next echo.HandlerFunc) echo.HandlerFunc {
-// 		return func (c echo.Context) error {
-// 			time_header := c.Request().Header.Get("X-timestamp")
-
-
-// 		}
-// 	}
-// }
+package service
