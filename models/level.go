0ac3da0b64d9689dd5e90199718fb1598f22e167
Sangiria <Kawaiinika@yandex.ru>
2025-06-07 16:13:12 +0300

    fix: исправление тегов в Prestige и Level

--- models/level.go
+++ models/level.go
@@ -2,7 +2,7 @@ package models
 
 type Level struct {
 	ID			uint	`json:"-" gorm:"primary_key"` 
-	Rank		uint	`json:"rank" gorm:"default:0 check:rank >= 0"`	
-	XP			float64	`json:"xp" gorm:"default:0 check:xp >= 0.0"`
+	Rank		uint	`json:"rank" gorm:"default:0;check:rank >= 0"`	
+	XP			float64	`json:"xp" gorm:"default:0;check:xp >= 0.0"`
 	SessionID 	uint	`json:"-"`	
 }
