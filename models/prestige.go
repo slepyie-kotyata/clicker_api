ec426472196454834aa5710f34bb39ac2204c579
Sangiria <Kawaiinika@yandex.ru>
2025-06-07 15:51:54 +0300

    fix: ограничения

--- models/prestige.go
+++ models/prestige.go
@@ -2,6 +2,6 @@ package models
 
 type Prestige struct {
 	ID				uint		`json:"-" gorm:"primary_key"`
-	CurrentValue 	float64		`json:"current_value" gorm:"default:0"`
+	CurrentValue 	float64		`json:"current_value" gorm:"default:0 check:current_value >= 0"`
 	SessionID       uint		`json:"-"`
 }
