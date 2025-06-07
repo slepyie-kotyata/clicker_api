e3025ca858f7959e6697685203942234f5ffe9e9
Sangiria <Kawaiinika@yandex.ru>
2025-06-07 16:10:11 +0300

    fix: исправление тегов

--- models/session.go
+++ models/session.go
@@ -4,8 +4,8 @@ type Session struct {
 	ID         		uint 		`json:"-" gorm:"primary_key"`
 	Money      		uint		`json:"money" gorm:"check:money >= 0"`
 	Dishes     		uint		`json:"dishes" gorm:"check:dishes >= 0"`
-	PrestigeValue 	float64		`json:"prestige_value" gorm:"check:prestige_value >= 0"`
-	PrestigeBoost   float64		`json:"-" gorm:"check:prestige_boost >= 0"`
+	PrestigeValue 	float64		`json:"prestige_value" gorm:"check:prestige_value >= 0.0"`
+	PrestigeBoost   float64		`json:"-" gorm:"check:prestige_boost >= 0.0"`
 	UserID     		uint		`json:"user_id"`
 	Level      		*Level		`json:"level" gorm:"foreignKey:SessionID"`
 	Prestige   		*Prestige   `json:"prestige" gorm:"foreignKey:SessionID"`
