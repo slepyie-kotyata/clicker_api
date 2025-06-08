70423048151d86a50957a710a48190f8d60cfe49
Sangiria <Kawaiinika@yandex.ru>
2025-06-08 21:57:30 +0300

    fix: исправлена ошибка в рутах

--- routes/session.go
+++ routes/session.go
@@ -8,7 +8,7 @@ import (
 
 func InitSessionRoutes(game *echo.Group) {
 	game.GET("/sessions", handlers.InitGame)
-	game.PATCH("/session/reset", handlers.SessionReset)
-	game.PATCH("/session/cook", handlers.CookClick)
-	game.PATCH("/session/sell", handlers.SellClick)
+	game.PATCH("/sessions/reset", handlers.SessionReset)
+	game.PATCH("/sessions/cook", handlers.CookClick)
+	game.PATCH("/sessions/sell", handlers.SellClick)
 }
