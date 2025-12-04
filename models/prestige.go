17fbcff67ab63fb013447e7a8d72aa05e3e05edb
sangiriah <lisaosadchenko@gmail.com>
2025-12-05 00:15:41 +0300

    feat: исправленная структура ответа

--- models/prestige.go
+++ models/prestige.go
@@ -1,7 +1,9 @@
 package models
 
 type Prestige struct {
-	ID				uint		`json:"-" gorm:"primary_key"`
-	CurrentValue 	float64		`json:"current_value" gorm:"default:0;check:current_value >= 0.0"`
-	SessionID       uint		`json:"-"`
+	ID					uint		`json:"-" gorm:"primary_key"`
+	CurrentValue 		float64		`json:"current_value" gorm:"default:0;check:current_value >= 0.0"`
+	CurrentBoostValue 	float64		`json:"current_boost_value" gorm:"default:0;check:prestige_value >= 0.0"`
+	AccumulatedValue   	float64		`json:"accumulated_value" gorm:"default:0;check:prestige_boost >= 0.0"`
+	SessionID       	uint		`json:"-"`
 }
