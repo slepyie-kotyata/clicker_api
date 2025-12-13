e7dc6f5e1307090f5e96e6866536952f66c514bd
sangiriah <lisaosadchenko@gmail.com>
2025-12-13 04:02:58 +0300

    fix: корректные имена полей для проверки в базе

--- models/prestige.go
+++ models/prestige.go
@@ -3,7 +3,7 @@ package models
 type Prestige struct {
 	ID					uint		`json:"-" gorm:"primary_key"`
 	CurrentValue 		float64		`json:"current_value" gorm:"default:0;check:current_value >= 0.0"`
-	CurrentBoostValue 	float64		`json:"current_boost_value" gorm:"default:0;check:prestige_value >= 0.0"`
-	AccumulatedValue   	float64		`json:"accumulated_value" gorm:"default:0;check:prestige_boost >= 0.0"`
+	CurrentBoostValue 	float64		`json:"current_boost_value" gorm:"default:0;check:current_boost_value >= 0.0"`
+	AccumulatedValue   	float64		`json:"accumulated_value" gorm:"default:0;check:accumulated_value >= 0.0"`
 	SessionID       	uint		`json:"-"`
 }
